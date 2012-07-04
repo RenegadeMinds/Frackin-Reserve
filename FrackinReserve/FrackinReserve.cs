fc2951749793ebff7841f57ed1d3d709e6a9176b
RenegadeMinds <ryan@renegademinds.com>
2012-07-04 17:03:58 +1000

    First local test from inside MonoDevelop.

--- FrackinReserve/FrackinReserve.cs
+++ FrackinReserve/FrackinReserve.cs
@@ -26,7 +26,7 @@ using OpenTK.Graphics.OpenGL;
 using System.Diagnostics;
 using OpenTK;
 using System.Runtime.InteropServices;
-// It's all in here.
+// It's all in here. And working.
 namespace FrackinReserve
 {
 	public partial class FrackinReserve : Gtk.Window
